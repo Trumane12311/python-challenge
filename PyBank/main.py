6f7ad6db18586ee00f8047e14ffeb346089ee249
Trumane12311 <Trumane12311@gmail.com>
2021-03-18 19:09:05 -0400

    Building PyBank Code Day 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,25 +2,49 @@ import os
 import csv
 
 #Set Path for the CSV File
-budgetdata = os.path.join("..","Resources","budget_data.csv")
+budgetdata = os.path.join('.., Resources, budget_data.csv')
 
 #Open CSV File
-with open(budgetdata) as csvfile:
-    csvreader = csv.reader(budgetdata, delimiter=",")
+with open(budgetdata, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    headers = next(csvreader)
+    months = str(row[1])
+    gainloss = int(row[2])
+    net_total = gainloss.sum()
+
 
 def print_percentages(budgetdata):
-    months = str(budgetdata[0])
+    monthspercent = str(budgetdata[0])
     lossgain = str(budgetdata[1])
+
+monthcount = ["Jan","Feb","Mar","April","May","Jun","Jul",
+            "Aug","Sep","October","November","December"]
+for months in monthcount:
+    print(month.count(monthcount))
+
 
 # * The total number of months included in the dataset
 print("Financial Analysis")
 print("---------------------------")
-print("Total Months: ")
+print(f"Total Months: " + str{monthcount})
   #The net total amount of "Profit/Losses" over the entire period
-print("Net Total: ")
+print("Net Total: " + str{net_total})
   #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-print("Average Change: ")
+print("Average Change: " + str{mean_change})
   #The greatest increase in profits (date and amount) over the entire period
-print("Greatest Increase in Profits: ")
+print("Greatest Increase in Profits: " + str{greatest_inc})
   # The greatest decrease in losses (date and amount) over the entire period
-print("Greatest Decrease in Losses: ")
+print("Greatest Decrease in Losses: " + str{greatest_dec})
+
+#Save the output file path
+output_file = os.path.join("..","analysis", "output.csv")
+
+with open(output_file, "w", newline='') as newbudgetfile:
+    csvwriter = csv.writer(newbudgetfile)
+    csvwriter.writerow("Financial Analysis")
+    csvwriter.writerow("-------------------")
+    csvwriter.writerow(f"Total Months:" + {total_months})
+    csvwriter.writerow(f"Net Total:" + str{net_total})
+    csvwriter.writerow(f"Average Change:" + str{mean_change})
+    csvwriter.writerow(f"Greatest Increase in Profits:" + str{greatest_inc})
+    csvwriter.writerow(f"Greatest Decrease in Profits:" + str{greatest_dec})
