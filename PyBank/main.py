78004016d1755216b11cb0a2ce0d31c6f85d1319
Trumane12311 <Trumane12311@gmail.com>
2021-03-18 19:58:11 -0400

    Modified PyBank Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from astropy.table import row
 
 #Set Path for the CSV File
 budgetdata = os.path.join('.., Resources, budget_data.csv')
@@ -8,33 +9,46 @@ budgetdata = os.path.join('.., Resources, budget_data.csv')
 with open(budgetdata, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     headers = next(csvreader)
-    months = str(row[1])
-    gainloss = int(row[2])
-    net_total = gainloss.sum()
+ 
+# * The total number of months included in the dataset
+print("Financial Analysis")
+print("---------------------------")
+
+def sum(numbers):
+    length = len(numbers)
+    monthcount = 0.0
+    for number in numbers:
+        monthcount += number
+
+print('Total Months: ') + print(sum(range(1)))
+
+#The net total amount of "Profit/Losses" over the entire period
+def sum(numbers):
+    length = len(numbers)
+    net_total = 0.0
+    for number in numbers:
+        net_total += number
+print(f'Net Total: ' + print(sum(range(2)))
+
+def average(numbers):
+    length = len(numbers)
+    mean_change = 0.0
+    for number in numbers:
+        mean_change += number
+    return mean_change / length
+  #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+print(f'Average Change: ' + '{mean_change}')
+
 
 
 def print_percentages(budgetdata):
     monthspercent = str(budgetdata[0])
     lossgain = str(budgetdata[1])
 
-monthcount = ["Jan","Feb","Mar","April","May","Jun","Jul",
-            "Aug","Sep","October","November","December"]
-for months in monthcount:
-    print(month.count(monthcount))
-
-
-# * The total number of months included in the dataset
-print("Financial Analysis")
-print("---------------------------")
-print(f"Total Months: " + str{monthcount})
-  #The net total amount of "Profit/Losses" over the entire period
-print("Net Total: " + str{net_total})
-  #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-print("Average Change: " + str{mean_change})
   #The greatest increase in profits (date and amount) over the entire period
-print("Greatest Increase in Profits: " + str{greatest_inc})
+print(f'Greatest Increase in Profits: ' + '{greatest_inc}')
   # The greatest decrease in losses (date and amount) over the entire period
-print("Greatest Decrease in Losses: " + str{greatest_dec})
+print(f"Greatest Decrease in Losses: " + '{greatest_dec}')
 
 #Save the output file path
 output_file = os.path.join("..","analysis", "output.csv")
@@ -43,8 +57,8 @@ with open(output_file, "w", newline='') as newbudgetfile:
     csvwriter = csv.writer(newbudgetfile)
     csvwriter.writerow("Financial Analysis")
     csvwriter.writerow("-------------------")
-    csvwriter.writerow(f"Total Months:" + {total_months})
-    csvwriter.writerow(f"Net Total:" + str{net_total})
-    csvwriter.writerow(f"Average Change:" + str{mean_change})
-    csvwriter.writerow(f"Greatest Increase in Profits:" + str{greatest_inc})
-    csvwriter.writerow(f"Greatest Decrease in Profits:" + str{greatest_dec})
+    csvwriter.writerow(f"Total Months:" + "{total_months}")
+    csvwriter.writerow(f"Net Total:" + "{net_total}")
+    csvwriter.writerow(f"Average Change:" + "{mean_change}")
+    csvwriter.writerow(f"Greatest Increase in Profits:" + "{greatest_inc}")
+    csvwriter.writerow(f"Greatest Decrease in Profits:" + "{greatest_dec}")
