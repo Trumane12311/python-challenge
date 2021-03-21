15f2fda32eaacc8ae4c00bbe5b7e65a2bc483432
Trumane12311 <Trumane12311@gmail.com>
2021-03-20 23:32:14 -0400

    Final Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -40,48 +40,7 @@ print(f'Net Profit: ${netprofit}')
 print(f'Average Change: ${averagechange}')
 print(f'Greatest Increase in Profits: {greatestinc[0]}, $({greatestinc[1]})')
 print(f'Greatest Decrease in Profits: {greatestdec[0]}, ${greatestdec[1]}')
- 
-# * The total number of months included in the dataset
-# print("Financial Analysis")
-# print("---------------------------")
 
-# def sum(numbers):
-#     length = len(numbers)
-#     monthcount = 0.0
-#     for number in numbers:
-#         monthcount += number
-
-# #print('Total Months: ') + print(sum(range(1)))
-
-# #The net total amount of "Profit/Losses" over the entire period
-# def sumgainloss(numbers):
-#     length = len(numbers)
-#     net_total = 0.0
-#     for number in numbers:
-#         net_total += number
-# print(f'Net Total: ' + print(sumgainloss(range(2)))
-
-# def average(numbers):
-#     length = len(numbers)
-#     mean_change = 0.0
-#     for number in numbers:
-#         mean_change += number
-#     return mean_change / length
-#   #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-# print(f'Average Change: ' + '{mean_change}')
-
-
-
-# def print_percentages(budgetdata):
-#     monthspercent = str(budgetdata[0])
-#     lossgain = str(budgetdata[1])
-
-#   #The greatest increase in profits (date and amount) over the entire period
-# print(f'Greatest Increase in Profits: ' + '{greatest_inc}')
-#   # The greatest decrease in losses (date and amount) over the entire period
-# print(f"Greatest Decrease in Losses: " + '{greatest_dec}')
-
-#Save the output file path
 output_file = os.path.join("analysis", "output.csv")
 
 with open(output_file, "w", newline='') as newbudgetfile:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,15 +18,30 @@ with open(polldata, 'r') as csvfile:
             candidate_votes[candidates] = 1
 
 total_votes = sum(candidate_votes.values())
-print(total_votes)
+print("Election Results")
+print("-------------------------")
+print(f'Total Votes: {total_votes}')
+print('-------------------------')
 
 percentage = []
 for i in candidate_votes:
     percentage = round((candidate_votes[i]/total_votes)*100,0)
-    print(f'{i} {percentage} {candidate_votes[i]}')
+    print(f'{i}: {percentage}% ({candidate_votes[i]})')
 
 for key in candidate_votes.keys():
     if candidate_votes[key] == max(candidate_votes.values()):
         winner = key
 
-print(f'{winner}')
+print(f'Winner: {winner}')
+
+output_path = os.path.join("analysis", "electiondata_summary.csv")
+
+with open(output_path, 'w', newline='') as electiondata_summary:
+  csvwriter = csv.writer(electiondata_summary, delimiter=',')
+  csvwriter.writerow(["Election Data Summary"])
+  csvwriter.writerow([f'Total Votes, {total_votes}'])
+  csvwriter.writerow(["Khan, 63.0%, (2218231)"])
+  csvwriter.writerow(["Correy, 20.0%, (704200)"])
+  csvwriter.writerow(["Li, 14.0%, (492940)"])
+  csvwriter.writerow(["O'Tooley, 3.0%, (105630)"])
+  csvwriter.writerow(['Winner, Khan'])
